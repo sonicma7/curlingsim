638115950df4a7d94cce9a7ba29c6ffeb8682d41
hotsho111 <hotsho111@00f45c13-810d-8779-12be-cb5d1d29b4c3>
2010-04-26 22:27:16 +0000

    Collisions and other minor code changes

--- rock.py
+++ rock.py
@@ -18,6 +18,7 @@ from pandac.PandaModules import Filename
 class Rock(DirectObject): #use to create player tank
     def __init__(self, color, world):
         self.world = world
+        self.color = color
         self.rock = loader.loadModel("art/Rock "+color+".egg")
         self.rock.setScale(1)
         self.rock.setZ(self.rock.getZ()+.7)
@@ -33,30 +34,32 @@ class Rock(DirectObject): #use to create player tank
         self.move = False
 
         self.keyMap = {"left":0, "right":0, "forward":0, "back":0, "headlight":0, "fire":0}
-        taskMgr.add(self.update, "Update")
-
+        
     def setkeyMap(self, keyMap):
         self.keyMap = keyMap        
 
-    def update(self, task):
+    def Update(self):
         dt = globalClock.getDt()
         
-        if self.world.keyMap["push"]:
-            self.move = True
+        ##if self.world.keyMap["push"]:
+        ##    self.move = True
     
             
-        if self.move == True:
-            normalforce = self.mass * self.gravity
-            frictionforce = normalforce * self.friction
-            acceleration = frictionforce / self.mass
-            self.yvelocity -= acceleration * dt
-            self.xvelocity = self.spin * self.radius * dt
-            
-            if(self.yvelocity > 0):
-                self.rock.setPos(self.rock.getX() + self.xvelocity, self.rock.getY()+ self.yvelocity, self.rock.getZ())
-            else:
-                print "Rock stopped"
-                return Task.done
+        #if self.move == True:
+        normalforce = self.mass * self.gravity
+        frictionforce = normalforce * self.friction
+        acceleration = frictionforce / self.mass
+        self.yvelocity -= acceleration * dt
+        self.xvelocity = self.spin * self.radius * 7 * dt
         
-        return Task.cont
+        if(self.yvelocity > 0):
+            self.rock.setPos(self.rock.getX() + self.xvelocity, self.rock.getY()+ self.yvelocity, self.rock.getZ())
+            self.rock.setH(self.rock.getH()-self.spin)
+        else:
+            self.yvelocity = 0
+            self.xvelocity = 0
+            #print "Rock stopped"
+            #return Task.done
+    
+        #return Task.cont
     
--- world.py
+++ world.py
@@ -15,17 +15,58 @@ import rock
 
 class World(DirectObject):
     def __init__(self):
-        self.rocks = rock.Rock("Red",self)
+        self.activeRocks = []
+        self.currentRock = rock.Rock("Red",self)
+        self.passiveRocks = [self.currentRock]        
         self.rink = loader.loadModel("art/Rink.egg")
         self.rink.setScale(1)
         self.rink.reparentTo(render)
+        self.turn = 1
         
         self.keyMap = {"push":0}  
 
-        self.accept("k", self.setKey, ["push", 1])    
-        self.accept("k-up", self.setKey, ["push", 0])
+        self.accept("k", self.pushRock)    
+        #self.accept("k-up", self.setKey, ["push", 0])
         self.accept('escape', sys.exit)
+        
+        taskMgr.add(self.update, "World-Update")
+
     
     def setKey(self, key, value):
         self.keyMap[key] = value
         
+    def pushRock(self):
+        self.activeRocks.append(self.currentRock)
+        if self.currentRock.color == "Red":
+            self.currentRock = rock.Rock("Yellow",self)
+        else:    
+            self.currentRock = rock.Rock("Red",self)
+        
+    def update(self, task):         
+        for i in self.activeRocks:
+            i.Update()
+
+        self.checkCollisions()
+        return task.cont
+        
+    def checkCollisions(self):
+        for i in self.activeRocks:
+            for j in self.activeRocks:
+                if i != j:
+                    ax = i.rock.getPos().getX()
+                    ay = i.rock.getPos().getY()
+                    az = i.rock.getPos().getZ()
+                    bx = j.rock.getPos().getX()
+                    by = j.rock.getPos().getY()
+                    bz = j.rock.getPos().getZ()
+                    dx = ax-bx
+                    dy = ay-by
+                    dz = az-bz
+                    distance = math.sqrt(dx*dx+dy*dy+dz*dz)
+                    if distance < 7*i.radius: 
+                        velocity = i.yvelocity + j.yvelocity
+                        i.yvelocity = velocity/2
+                        j.yvelocity = velocity/2
+                        
+    def computeDistance(a,b):
+        pass
